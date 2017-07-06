52551205778ca6db16a0b82b18461173c9e59529
Soleil Rojas <soleilrojas@outlook.com>
2017-07-06 12:49:44 -0600

    Added color selector

--- XpBar/configuration.lua
+++ XpBar/configuration.lua
@@ -1,3 +1,57 @@
+local function PresentColorEditor(label, col, col_d)
+    local changed = false
+    local i =
+    {
+        bit.band(bit.rshift(col, 16), 0xFF),
+        bit.band(bit.rshift(col, 8), 0xFF),
+        bit.band(col, 0xFF),
+        bit.band(bit.rshift(col, 24), 0xFF),
+    }
+
+    local ids = { "##X", "##Y", "##Z", "##W" }
+    local fmt = { "R:%3.0f", "G:%3.0f", "B:%3.0f", "A:%3.0f" }
+
+    imgui.BeginGroup()
+    imgui.PushID(label)
+
+    imgui.PushItemWidth(50)
+    for n = 1, 4, 1 do
+        local changedDragInt = false
+        if n ~= 1 then
+            imgui.SameLine(0, 5)
+        end
+        
+        changedDragInt, i[n] = imgui.DragInt(ids[n], i[n], 1.0, 0, 255, fmt[n])
+    end
+    imgui.PopItemWidth()
+
+    imgui.SameLine(0, 5)
+    imgui.ColorButton(i[1] / 255, i[2] / 255, i[3] / 255, 1.0)
+    if imgui.IsItemHovered() then
+        imgui.SetTooltip(
+            string.format(
+                "Color:\n#%02X%02X%02X%02X",
+                i[1], i[2], i[3], i[4]
+            )
+        )
+    end
+
+    imgui.SameLine(0, 5)
+    imgui.Text(label)
+
+    col = bit.lshift(i[4], 24) + bit.lshift(i[1], 16) +  bit.lshift(i[2], 8) +  i[3]
+
+    imgui.SameLine(0, 5)
+    if imgui.Button("Reset") then
+        col = col_d
+    end
+
+    imgui.PopID()
+    imgui.EndGroup()
+
+    return col
+end
+
 local function ConfigurationWindow(configuration)
     local this = 
     {
@@ -45,8 +99,11 @@ local function ConfigurationWindow(configuration)
             this.changed = true
         end
 
-        
-    
+        local oldColor = _configuration.xpBarColor
+        _configuration.xpBarColor = PresentColorEditor("XP bar color", _configuration.xpBarColor, 0xFFE6B300)
+        if oldColor ~= _configuration.xpBarColor then
+            this.changed = true
+        end
     end
 
     this.Update = function()
